8829149e080265f89b1c70a06b24c62527d7d4f2
Dal <Dalianhayes@yahoo.com>
2016-05-11 16:42:00 -0400

    Update Test.js

--- Script/Test.js
+++ Script/Test.js
@@ -12,3 +12,24 @@
 		die3.innerHTML = d3;
 		status.innerHTML = "You rolled "+diceTotal+".";
 	}
+	
+		/* JavaScript for NeraRecords.Html (index)*/
+	document.write("Last modified on "+document.lastModified);
+
+
+	/* Code for Clock */
+		function runClock() {
+		now = new Date();
+		hours = now.getHours();
+		minutes = now.getMinutes();
+		seconds = now.getSeconds();
+		timeStr = "" + ((hours > 12) ? hours - 12 : hours);
+		timeStr += ((minutes < 10) ? ":0" : ":") + minutes;
+		timeStr += ((seconds < 10) ? ":0" : ":") + seconds;
+		timeStr += (hours >= 12) ? " PM" : " AM";
+		document.clock.face.value=timeStr;
+		setTimeout("runClock()",1000);
+	}
+
+		/* Code and Programing by:
+			:Dalian Hayes~ */
