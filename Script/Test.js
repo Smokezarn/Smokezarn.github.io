5b83994c96ada70efe46240d4b929f632399741a
Dal <Dalianhayes@yahoo.com>
2016-05-11 16:45:33 -0400

    Update Test.js

--- Script/Test.js
+++ Script/Test.js
@@ -13,20 +13,5 @@
 		status.innerHTML = "You rolled "+diceTotal+".";
 	}
 
-
-	/* Code for Clock */
-		function runClock() {
-		now = new Date();
-		hours = now.getHours();
-		minutes = now.getMinutes();
-		seconds = now.getSeconds();
-		timeStr = "" + ((hours > 12) ? hours - 12 : hours);
-		timeStr += ((minutes < 10) ? ":0" : ":") + minutes;
-		timeStr += ((seconds < 10) ? ":0" : ":") + seconds;
-		timeStr += (hours >= 12) ? " PM" : " AM";
-		document.clock.face.value=timeStr;
-		setTimeout("runClock()",1000);
-	}
-
 		/* Code and Programing by:
 			:Dalian Hayes~ */
