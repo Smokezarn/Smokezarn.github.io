3ba486e77142044d6ae374fdf14e90a66c959310
Dal <Dalianhayes@yahoo.com>
2016-08-08 01:13:32 -0400

    Update Test.js

--- Script/Test.js
+++ Script/Test.js
@@ -26,21 +26,5 @@ function runClock() {
 }
 
 
-	/* Code for Dice roller! */
-	function rollDice(){
-		var die1 = document.getElementById("Die1");
-		var die2 = document.getElementById("Die2");
-		var die3 = document.getElementById("Die3");
-		var status = document.getElementById("status");
-		var d1 = Math.floor(Math.random() * 10) + 1;
-		var d2 = Math.floor(Math.random() * 10) + 1;
-		var d3 = Math.floor(Math.random() * 10) + 1;
-		var diceTotal = d1 + d2 + d3;
-		die1.innerHTML = d1;
-		die2.innerHTML = d2;
-		die3.innerHTML = d3;
-		status.innerHTML = "You rolled "+diceTotal+".";
-	}
-
 		/* Code and Programing by:
 			:Dalian Hayes~ Source Help from CodeAcademy.com */
