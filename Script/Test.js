aa4ee3d68173c7d033ca352100a3d8ac4486f43f
Dal <Dalianhayes@yahoo.com>
2016-08-23 12:56:49 -0400

    Fixed Clock

--- Script/Test.js
+++ Script/Test.js
@@ -24,7 +24,26 @@ function runClock() {
 
 	setTimeout("runClock()",1000);
 }
+	window.onload = function() {
+			runClock();
+		}
 
+
+	/* Code for Dice roller! */
+	function rollDice(){
+		var die1 = document.getElementById("Die1");
+		var die2 = document.getElementById("Die2");
+		var die3 = document.getElementById("Die3");
+		var status = document.getElementById("status");
+		var d1 = Math.floor(Math.random() * 100) + 1;
+		var d2 = Math.floor(Math.random() * 10) + 1;
+		var d3 = Math.floor(Math.random() * 10) + 1;
+		var diceTotal = d1 + d2 + d3;
+		die1.innerHTML = d1;
+		die2.innerHTML = d2;
+		die3.innerHTML = d3;
+		status.innerHTML = "You rolled "+diceTotal+".";
+	}
 
 		/* Code and Programing by:
 			:Dalian Hayes~ Source Help from CodeAcademy.com */
