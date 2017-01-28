000643c237a6de34ed52dd5a471c905a41ce4961
Dal~ <Dalianhayes@yahoo.com>
2017-01-28 12:59:46 -0500

    Revert "Moved Email Form to About Us Page.- Small code edits"
    This reverts commit e268a673cc5070b50ec4454e8a70cb5ec8036574.

---
+++ Script/Test.js
@@ -0,0 +1,48 @@
+	/* JavaScript for NeraRecords.Html (index)*/
+
+	/* Code for Clock */
+function runClock() {
+
+	now = new Date();
+
+	hours = now.getHours();
+
+	minutes = now.getMinutes();
+
+	seconds = now.getSeconds();
+
+	timeStr = "" + ((hours > 12) ? hours - 12 : hours);
+
+	timeStr += ((minutes < 10) ? ":0" : ":") + minutes;
+
+	timeStr += ((seconds < 10) ? ":0" : ":") + seconds;
+
+	timeStr += (hours >= 12) ? " PM" : " AM";
+
+	document.clock.face.value=timeStr;
+
+	setTimeout("runClock()",1000);
+}
+	window.onload = function() {
+			runClock();
+		}
+
+
+	/* Code for Dice roller! */
+	function rollDice(){
+		var die1 = document.getElementById("Die1");
+		var die2 = document.getElementById("Die2");
+		var die3 = document.getElementById("Die3");
+		var status = document.getElementById("status");
+		var d1 = Math.floor(Math.random() * 100) + 1;
+		var d2 = Math.floor(Math.random() * 10) + 1;
+		var d3 = Math.floor(Math.random() * 10) + 1;
+		var diceTotal = d1 + d2 + d3;
+		die1.innerHTML = d1;
+		die2.innerHTML = d2;
+		die3.innerHTML = d3;
+		status.innerHTML = "You rolled "+diceTotal+".";
+	}
+
+	/* © Copyright Dalian Hayes, 2015-2016~ Source Help from CodeAcademy.com 
+					Detailed Copyright Notice at :http://Smokezarn.github.io/Pages/CRN */
