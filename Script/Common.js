e7aad308b5d73c7e8afe106714d77fb19c9b560a
Dal~ <Dalianhayes@yahoo.com>
2017-02-19 02:32:50 -0500

    Fixed Dropdwns And Local Optimizations
    *Fixed the 404 page to work better on local machine
    *Edited the Common.js File to hide Dropdowns.
    -Removed Javascript files from most all pages to fall base on Common.js

--- Script/Common.js
+++ Script/Common.js
@@ -1,12 +1,18 @@
-/* Common JavaScript that all my pages use */
+//Common JavaScript that all my pages use
 
-		/* Script for Dropdown Menu */
-/*toggle between hiding and showing the dropdown content */
+		// Script for Dropdown Menu
+//toggle between hiding and showing the dropdown content
 function myFunction1() {document.getElementById("myDropdown1").classList.toggle("show1");}
 function myFunction2() {document.getElementById("myDropdown2").classList.toggle("show2");}
 function myFunction3() {document.getElementById("myDropdown3").classList.toggle("show3");}
+		// Close the dropdown menu if mouse click out
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) { var dropdowns = document.getElementsByClassName("dropdown-content"); var i;for (i = 0; i < dropdowns.length; i++) {var openDropdown = dropdowns[i];
+	  if (openDropdown.classList.contains('show1')) {openDropdown.classList.remove('show1');}
+	  if (openDropdown.classList.contains('show2')) {openDropdown.classList.remove('show2');}
+	  if (openDropdown.classList.contains('show3')) {openDropdown.classList.remove('show3');} }}}
 
-	/* Script for Clock */
+	//Script for Clock
 function runClock() {
 	now = new Date();
 	hours = now.getHours();
@@ -19,11 +25,13 @@ function runClock() {
 	document.clock.face.value=timeStr;
 	setTimeout("runClock()",1000);
 } window.onload = function() {runClock();}
-/* Hide Body Scroll Bar */
+
+	// Hide Body Scroll Bar
 	 var body = document.getElementsByTagName('body')[0];
 		function disableBodyScroll() {body.style.overflowY = 'hidden';}
 		function enableBodyScroll() {body.style.overflowY = 'auto';}
-/* Script for Dice roller! */
+		
+	// Script for Dice roller!
 function rollDice(){
 	var die1 = document.getElementById("Die1");
 	var die2 = document.getElementById("Die2");
